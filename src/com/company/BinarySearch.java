b19bb6d0ef55fdcf30d46520af686990ab30685c
wgl <wanggulin@zykjct.com>
2019-11-17 15:33:00 +0800

    实验练习部分

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 
 import java.util.Arrays;
---
+++ src/com/practice/basis_1_38.java
@@ -0,0 +1,7 @@
+package com.practice;
+
+/**
+ * @deprecated 骰子模拟
+ */
+public class basis_1_38 {
+}
